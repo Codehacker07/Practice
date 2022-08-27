b26076b710fd978b1e6a9366c11ad697c465572b
Purushotham Reddy <91183766+Codehacker07@users.noreply.github.com>
2022-08-27 13:19:46 +0530

    Update VTF.py

--- VTF.py
+++ VTF.py
@@ -1,6 +1,6 @@
 import pyttsx3
 friend = pyttsx3.init()
-speech = input ("Say So,Something: ")
+speech = input ("Say Something: ")
 friend.say("Hello , Good morning")
 friend.say(speech)
 friend.runAndWait()
